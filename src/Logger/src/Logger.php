97fc184c360a4eea23379de1859388714c23bb88
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2024-05-01 10:39:15 -0500

    [Logger] Logger Determines Which Logger to Use and Passed The Log Message and Context to the Appropriate Logger

--- src/Logger/src/Logger.php
+++ src/Logger/src/Logger.php
@@ -2,61 +2,81 @@
 
 namespace PHPGenesis\Logger;
 
-class Logger extends BaseLogger implements ILogger
+use PHPGenesis\Common\Composer\Composer;
+use PHPGenesis\Logger\Loggers\LaravelLogger;
+use PHPGenesis\Logger\Loggers\MonoLogger;
+
+class Logger implements ILogger
 {
     public static function debug(string $message, ?array $context = []): void
     {
-        $logger = new Logger();
-
-        $logger->log(LogLevel::DEBUG, $message, $context);
+        if (Composer::installed('laravel/framework') || Composer::installed('laravel/support')) {
+            LaravelLogger::debug($message, $context);
+        } else {
+            MonoLogger::debug($message, $context);
+        }
     }
 
     public static function info(string $message, ?array $context = []): void
     {
-        $logger = new Logger();
-
-        $logger->log(LogLevel::INFO, $message, $context);
+        if (Composer::installed('laravel/framework') || Composer::installed('laravel/support')) {
+            LaravelLogger::info($message, $context);
+        } else {
+            MonoLogger::info($message, $context);
+        }
     }
 
     public static function notice(string $message, ?array $context = []): void
     {
-        $logger = new Logger();
-
-        $logger->log(LogLevel::NOTICE, $message, $context);
+        if (Composer::installed('laravel/framework') || Composer::installed('laravel/support')) {
+            LaravelLogger::notice($message, $context);
+        } else {
+            MonoLogger::notice($message, $context);
+        }
     }
 
     public static function warning(string $message, ?array $context = []): void
     {
-        $logger = new Logger();
-
-        $logger->log(LogLevel::WARNING, $message, $context);
+        if (Composer::installed('laravel/framework') || Composer::installed('laravel/support')) {
+            LaravelLogger::warning($message, $context);
+        } else {
+            MonoLogger::warning($message, $context);
+        }
     }
 
     public static function error(string $message, ?array $context = []): void
     {
-        $logger = new Logger();
-
-        $logger->log(LogLevel::ERROR, $message, $context);
+        if (Composer::installed('laravel/framework') || Composer::installed('laravel/support')) {
+            LaravelLogger::error($message, $context);
+        } else {
+            MonoLogger::error($message, $context);
+        }
     }
 
     public static function critical(string $message, ?array $context = []): void
     {
-        $logger = new Logger();
-
-        $logger->log(LogLevel::CRITICAL, $message, $context);
+        if (Composer::installed('laravel/framework') || Composer::installed('laravel/support')) {
+            LaravelLogger::critical($message, $context);
+        } else {
+            MonoLogger::critical($message, $context);
+        }
     }
 
     public static function alert(string $message, ?array $context = []): void
     {
-        $logger = new Logger();
-
-        $logger->log(LogLevel::ALERT, $message, $context);
+        if (Composer::installed('laravel/framework') || Composer::installed('laravel/support')) {
+            LaravelLogger::alert($message, $context);
+        } else {
+            MonoLogger::alert($message, $context);
+        }
     }
 
     public static function emergency(string $message, ?array $context = []): void
     {
-        $logger = new Logger();
-
-        $logger->log(LogLevel::EMERGENCY, $message, $context);
+        if (Composer::installed('laravel/framework') || Composer::installed('laravel/support')) {
+            LaravelLogger::emergency($message, $context);
+        } else {
+            MonoLogger::emergency($message, $context);
+        }
     }
 }

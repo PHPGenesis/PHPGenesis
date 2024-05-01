eb724d38a4c34a8dbf4698cb33b0badd00050fbb
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2024-05-01 10:38:37 -0500

    [Logger] Create LaravelLogger

---
+++ src/Logger/src/Loggers/LaravelLogger.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace PHPGenesis\Logger\Loggers;
+
+use Illuminate\Support\Facades\Log;
+use PHPGenesis\Logger\ILogger;
+
+/**
+ * @api
+ * @internal This class is not meant to be used by developers outside of the PHPGenesis\Logger namespace.
+ */
+class LaravelLogger implements ILogger
+{
+    public static function debug($message, $context = []): void
+    {
+        Log::debug($message, $context);
+    }
+
+    public static function info($message, $context = []): void
+    {
+        Log::info($message, $context);
+    }
+
+    public static function notice($message, $context = []): void
+    {
+        Log::notice($message, $context);
+    }
+
+    public static function warning($message, $context = []): void
+    {
+        Log::warning($message, $context);
+    }
+
+    public static function error($message, $context = []): void
+    {
+        Log::error($message, $context);
+    }
+
+    public static function critical($message, $context = []): void
+    {
+        Log::critical($message, $context);
+    }
+
+    public static function alert($message, $context = []): void
+    {
+        Log::alert($message, $context);
+    }
+
+    public static function emergency($message, $context = []): void
+    {
+        Log::emergency($message, $context);
+    }
+}

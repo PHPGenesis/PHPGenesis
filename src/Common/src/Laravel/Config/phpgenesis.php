ca5b56157ab29764f95738d6f030399d0f5499a8
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2024-05-02 15:50:59 -0500

    [Common] Create Base Laravel Config File

---
+++ src/Common/src/Laravel/Config/phpgenesis.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'amazonWebServices' => [
+        'credentials' => [
+            'key' => 'your-key',
+            'secret' => 'your-secret',
+        ],
+    ],
+    'logger' => [
+        'name' => 'phpgenesis',
+        'logFileName' => 'phpgenesis.log',
+        'logLevel' => 'debug',
+    ],
+];

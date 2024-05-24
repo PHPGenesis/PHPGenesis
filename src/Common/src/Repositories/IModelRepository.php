1a8b5c88b54766673261f97a3766af3fe7bffb43
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2024-05-24 08:20:13 -0500

    [Common] Create IModelRepository

---
+++ src/Common/src/Repositories/IModelRepository.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Copyright (c) 2024. Encore Digital Group.
+ * All Right Reserved.
+ */
+
+namespace PHPGenesis\Common\Repositories;
+
+interface IModelRepository
+{
+    public function create(array $attributes = []);
+
+    public function firstOrCreate(array $attributes = [], array $values = []);
+
+    public function all();
+
+    public function find(string|int $id, array $columns = ['*']);
+
+    public function get(array $columns = ['*']);
+
+    public function update(array $values);
+
+    public function updateOrCreate(array $attributes = [], array $values = []);
+
+    public function delete();
+}
